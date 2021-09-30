255ce3b8664c5d53bc891a45ce5fb988780763ff
Ruchira Sumithra Arachchi <ruchz@live.com>
2021-09-30 18:08:30 +0800

    Added if examples to main

--- IA2211Lesson02/src/com/fotuoc/Main.java
+++ IA2211Lesson02/src/com/fotuoc/Main.java
@@ -3,7 +3,43 @@ package com.fotuoc;
 
 public class Main {
 
+    static String name = "Ruchira";
+    public static final int serialNum = 10;
+    boolean isActive = false;
+
+
     public static void main(String[] args) {
-	// write your code here
+        System.out.println("Hello World!");
+        System.out.println("Name: " + name);
+
+        String myName = "Ruchira" + " " + "Thilanka";
+        System.out.println(myName);
+
+        int valA = 10;
+        double valB = (double) valA;
+
+        // Specific reason
+
+        int valC = valA / (int) 2.5;
+        System.out.println("valC = " + valC);
+
+        // User param
+        int age = 88;
+
+        if (age < 18) {
+            System.out.println("Not eligible for a driving license");
+        }
+        else if (age > 80) {
+            System.out.println("Too old to drive. Please submit a health check results");
+        }
+        else {
+            System.out.println("Allowed to apply");
+        }
+
+        switch (age){
+            case 80:
+
+
+        }
     }
 }
